9154b694d87c026b179b35bbd6161f60328badfd
yoonet-developer <hi@yoonet.io>
2025-09-09 15:38:25 +0800

    updates

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qGI+bIYhl+j8ATSG6NHmoFgaRDzNFlmiXPaiKXrfBBc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"igzD5Ls94Zrwoth/jBaajTk16nZEmkliGWcWJyvwKrA=\"}"
