0beb71f11ff7ab744221123dd3aec3b4e869fa2f
yoonet-developer <hi@yoonet.io>
2025-09-12 14:29:01 +0800

    fix

--- app/api/auth/custom-signin/route.ts
+++ app/api/auth/custom-signin/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import bcrypt from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
-import { signIn } from '@/lib/auth'
+import { prisma } from '../../../../lib/prisma'
 
 export async function POST(request: Request) {
   try {
